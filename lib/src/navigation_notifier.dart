cf00b32bcf6c365476b93cc544a042d4f6e3c24e
Kristián Balaj <kiko.balaj@gmail.com>
2021-10-19 11:21:36 +0200

    style: remove unused dep

--- lib/src/navigation_notifier.dart
+++ lib/src/navigation_notifier.dart
@@ -1,6 +1,5 @@
 import 'dart:collection';
 
-import 'package:collection/collection.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
